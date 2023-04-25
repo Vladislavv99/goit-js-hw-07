a77ad44f7b39cfaee409cbb9f7c519b68a948e86
Vladislavv99 <vladlegur6@gmail.com>
2023-04-25 18:44:56 +0300

    comit

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,29 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const ulList = document.querySelector(".gallery");
 
-console.log(galleryItems);
+const markup = galleryItems
+  .map(({ preview, original, description }) => {
+    return `
+  <li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>
+    `;
+  })
+  .join("");
+ulList.insertAdjacentHTML("beforeend", markup);
+
+ulList.addEventListener("click", onImageClick);
+function onImageClick(e) {
+  if (e.target.nodeName !== "IMG") {
+    return;
+  }
+  console.log("hello");
+}
